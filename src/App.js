c0730c0569177cfdab7be29dd3b9bbd4533e77df
Akash Singh <singhakash89890@gmail.com>
2023-12-26 12:45:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-import UserDirectory from './UserDirectory';
-import UserDetails from './UserDetails';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" exact element={<><UserDirectory/></>} />
-        <Route path="/user/:userId" element={<UserDetails/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      
+      <Footer />
     </Router>
   );
 }
